9766a837cb712aacc7f4f40c97ea59b0823f54de
Syed Ali Faraz <alii.exe3@gmail.com>
2026-08-19 07:39:36 +0500

    Update API endpoint to use environment variable for query requests

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -53,7 +53,7 @@ export default function Home() {
     setActiveRepos([]);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/query", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/query`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
